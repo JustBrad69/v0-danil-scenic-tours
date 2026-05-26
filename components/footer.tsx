3fac163d9a56574c8ce25a4fbe71d31c458aa03c
JustBrad69 <bradisjust@gmail.com>
2026-05-26 13:50:18 +0300

    Update footer address to remove Githurai

--- components/footer.tsx
+++ components/footer.tsx
@@ -58,7 +58,7 @@ export default function Footer() {
               </div>
               <div>
                 <p className="opacity-75">Address</p>
-                <p>Northern Bypass Rd, Githurai, Nairobi, Kenya</p>
+                <p>Northern Bypass Rd, Nairobi, Kenya</p>
               </div>
               <div>
                 <p className="opacity-75 mb-2">Email Us</p>
